e50fc27a60427d5c886472c78cf9ed8c6ef7befa
Vrukshali Torawane <65615660+Vrukshali-26@users.noreply.github.com>
2021-12-03 09:20:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "civo_kubernetes_cluster" "cluster" {
-    name = "sample"
+    name = "Argo-Cluster"
     applications = "-Traefik,-metrics-server,argo-cd"
     num_target_nodes = 3
     target_nodes_size = "g3.k3s.medium"
